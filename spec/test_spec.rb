4010a16b6b998a7d69b61d90907c3d4ee24dd717
Manmeet Saluja <manmeet@workato.com>
2014-11-19 15:16:19 -0800

    Update test_spec.rb

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 require_relative "../lib/hello.rb"
 
 describe "hello" do
-  it "should display 2" do
+  it "should display ack message from node app" do
     expect(Hello.new.ping).to eq('ack')
   end
 end
